0fbdebf28ad4228caf3210a84f8bf1a0946e4471
AbdoulSalam407 <abdoulsalamd742@gmail.com>
2025-12-06 19:37:04 +0000

    fix(tickets): afficher tous les tickets au lieu de filtrer par utilisateur

--- backend/tickets/views.py
+++ backend/tickets/views.py
@@ -4,11 +4,9 @@ from .models import Ticket
 from .serializers import TicketSerializer
 
 class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
     permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        return Ticket.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
